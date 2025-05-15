9cc8c5102fd4c9333d89659d4e818f5deb928a8d
gomowele <godwinjr813@gmail.com>
2025-05-15 12:54:31 -0400

    Delete main.jsx

--- main.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import QuizGame from './QuizGame';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<QuizGame />);
