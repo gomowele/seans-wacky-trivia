4d6613cf75b1ed43adb7cf1e4f0c8174b8baa55b
gomowele <godwinjr813@gmail.com>
2025-05-16 09:14:03 -0400

    Create App.jsx

---
+++ seans-wacky-trivia-complete/frontend/src/App.jsx
@@ -0,0 +1,42 @@
+// src/App.jsx
+import React, { useState, useEffect } from 'react';
+import QuizGame from './QuizGame';
+import Lobby from './Lobby';
+
+export default function App() {
+  const [nickname, setNickname] = useState('');
+  const [icon, setIcon] = useState('');
+
+  useEffect(() => {
+    const savedNick = localStorage.getItem('nickname');
+    const savedIcon = localStorage.getItem('icon');
+    if (savedNick && savedIcon) {
+      setNickname(savedNick);
+      setIcon(savedIcon);
+    }
+  }, []);
+
+  if (!nickname || !icon) {
+    return (
+      <Lobby
+        onSubmit={(nick, icn) => {
+          setNickname(nick);
+          setIcon(icn);
+          localStorage.setItem('nickname', nick);
+          localStorage.setItem('icon', icn);
+        }}
+      />
+    );
+  }
+
+  return (
+    <QuizGame
+      nickname={nickname}
+      icon={icon}
+      onReset={() => {
+        localStorage.clear();
+        window.location.reload();
+      }}
+    />
+  );
+}
