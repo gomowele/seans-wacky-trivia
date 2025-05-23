3b67d0fa45932673db1ae6697f7361f0f845390c
gomowele <godwinjr813@gmail.com>
2025-05-23 19:53:58 -0400

    Update App.jsx

--- seans-wacky-trivia-complete/frontend/src/App.jsx
+++ seans-wacky-trivia-complete/frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import Lobby from './Lobby';
 import QuizGame from './QuizGame';
 import './QuizGame.css';
@@ -6,7 +6,6 @@ import './QuizGame.css';
 export default function App() {
   const [nickname, setNickname] = useState('');
   const [icon, setIcon] = useState('');
-  const waitingMusicRef = useRef(null);
 
   useEffect(() => {
     const savedNick = localStorage.getItem('nickname');
@@ -17,26 +16,16 @@ export default function App() {
     }
   }, []);
 
-  // Play waiting music on load
-  useEffect(() => {
-    if (!nickname || !icon) {
-      waitingMusicRef.current?.play().catch(err => console.log('Music error:', err));
-    }
-  }, [nickname, icon]);
-
   if (!nickname || !icon) {
     return (
-      <>
-        <Lobby
-          onSubmit={(nick, icn) => {
-            setNickname(nick);
-            setIcon(icn);
-            localStorage.setItem('nickname', nick);
-            localStorage.setItem('icon', icn);
-          }}
-        />
-        <audio ref={waitingMusicRef} src="/music/waiting screen.mp3" loop autoPlay />
-      </>
+      <Lobby
+        onSubmit={(nick, icn) => {
+          setNickname(nick);
+          setIcon(icn);
+          localStorage.setItem('nickname', nick);
+          localStorage.setItem('icon', icn);
+        }}
+      />
     );
   }
 
