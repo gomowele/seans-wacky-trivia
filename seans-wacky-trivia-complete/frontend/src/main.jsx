9abcf9c3474dae5a99cee4a2b6b8312f0f830eb3
gomowele <godwinjr813@gmail.com>
2025-05-23 12:47:30 -0400

    Update main.jsx

--- seans-wacky-trivia-complete/frontend/src/main.jsx
+++ seans-wacky-trivia-complete/frontend/src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
-import './QuizGame.css';
+import './index.css'; // Global styles
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
